36206f43d5541910f6e2a5a40cc1065e9ed1fc01
jahan <jwalsh370@gmail.com>
2017-03-03 12:51:58 -0800

    add debugger to console, add function to add value of checked boxes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,62 @@
-function Pizza(size, toppings) {
-  this.size = [];
-  this.toppings = [];
+//business logic
+debugger;
+function PizzaValue(amountValue, toppingValue) {
+  this.amountValue = amountvalue;
+  this.toppingValue = toppingValue;
 }
 
-function PizzaOrder(pizzaType, pizzaAmount) {
-  this.pizzaType = pizzaType;
-  this.pizzaAmount = pizzaAmount;
-}
+  var toppingValue = 0;
+  var inputElements = document.getElementsByClassName('pizzaTop');
+  for(var i=0; inputElements[i]; ++i){
+        if(inputElements[i].checked){
+             toppingValue = inputElements[i].value;
 
-var pizzaSize = { size: ["small", "medium", "large"]}
-var pizzaToppings = { toppings: ["pepperoni", "sausage", "Fresh Mozerella", "kale", "roasted onions", "roasted potatos", "hot peppers"]}
+        }
+  }
+
+  var amountValue = 0;
+  var inputElements = document.getElementsByClassName('sizeSelector');
+  for(var i=0; inputElements[i]; ++i){
+        if(inputElements[i].checked){
+             amountValue = inputElements[i].value;
+
+        }
+  }
+  var pizzaTime = document.getElementById("pizzaTime")
+function clickCh(caller){
+if(caller.checked){
+add(caller)
+} else {
+subtract(caller)
+}
+}
+function add(caller){ total.value = total.value*1 + caller.value*1}
+function subtract(caller){ total.value = total.value*1 - caller.value*1}
+
+console.log(amountValue);
+console.log(toppingValue);
+console.log(PizzaValue);
+ $(document).ready(function() {
+console.log(pizzaTime);
+
+// var selectedPizzaCount = 0;
+// var selectedPizzaType = "";
+//
+//
+//   $(".pizzaPicker").on("change keyup", function() {
+//     $("#totalCost").val(function() {
+//         var toppingVal = parseInt($(this).siblings(":checked").val());
+//         var countVal = parseInt($("#min").val()) || 0;
+//
+//         return toppingVal + countVal;
+//     });
+// });
+//
+//
+//
+  $("form.pizzaPicker").submit(function(event) {
+    event.preventDefault();
+//
+  });
+//
+ });
