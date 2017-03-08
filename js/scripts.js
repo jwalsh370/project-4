c4a03d0dcbdaf26130684f339ada6d0159b610a5
jwalsh370 <jwalsh370@gmail.com>
2017-03-07 17:28:12 -0800

    add fixedTo function to return the value of final price with decimal to the second digit. Delete spacing in html and css and clean up indentation.

--- js/scripts.js
+++ js/scripts.js
@@ -11,16 +11,15 @@ function PapiPizza(pizzaSize, pizzaToppings) {
   PapiPizza.prototype.calculateSizePrice = function() {
       sizeCost = 0;
       if (this.size === "8") {
-    sizeCost = 8;
+    sizeCost = 8.00;
   } else if (this.size === "16") {
-    sizeCost = 16;
+    sizeCost = 16.00;
   }
   }
 
   PapiPizza.prototype.calculatePizzaPrice = function() {
-     return (this.pizzaToppings.length * 2 + this.pizzaSize);
+     return (this.pizzaToppings.length * 2.00 + this.pizzaSize).toFixed(2);
 }
-
 
 // front end
 $(document).ready(function() {
@@ -38,14 +37,11 @@ $(document).ready(function() {
      }
    });
 
-
    newPapiPizza = new PapiPizza(pizzaSize, finalToppings);
-   $("#pizzaOutput").append("<p> Papi Thanks you for choosing us, the total comes too: $" + newPapiPizza.calculatePizzaPrice() + "</p>");
+   $("#pizzaOutput").append("<p> Papi thanks you for choosing us, your total: $" + newPapiPizza.calculatePizzaPrice() + "</p>");
       finalToppings.forEach(function(finalPizzaTopping){
         $("#toppingOutput").append("<li>" + finalPizzaTopping + "</li>");
           $(".finalToppings").show();
       });
-      console.log(newPapiPizza);
-      console.log(pizzaSize);
   });
 });
