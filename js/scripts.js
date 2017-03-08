463f27faf35d288ca810fd67632dfdfc8b6a65c4
jwalsh370 <jwalsh370@gmail.com>
2017-03-07 17:06:34 -0800

    fix js function to append final price output on html.

--- js/scripts.js
+++ js/scripts.js
@@ -1,78 +1,51 @@
 
 // back end
-var finalCost = 0;
-var toppingPrice = 0;
-var pizzaSizePrice = 0;
-var pizzaSize
-var pizzaTops1
-var newPapiPizza
-var finalPizza
-var finalToppings
+sizeCost = 0;
 
-function PapiPizza(pizzaSize) {
-  this.pizzaToppings = [];
+function PapiPizza(pizzaSize, pizzaToppings) {
+  this.pizzaToppings = pizzaToppings;
   this.pizzaSize = pizzaSize;
 
   }
 
-PapiPizza.prototype.calculateSize = function() {
-  if (this.pizzaSize === "small") {
-     pizzaSizePrice = 8;
-  } else if (this.pizzaSize === "large") {
-         pizzaSizePrice = 16;
+  PapiPizza.prototype.calculateSizePrice = function() {
+      sizeCost = 0;
+      if (this.size === "8") {
+    sizeCost = 8;
+  } else if (this.size === "16") {
+    sizeCost = 16;
   }
-  return pizzaSizePrice;
+  }
+
+  PapiPizza.prototype.calculatePizzaPrice = function() {
+     return (this.pizzaToppings.length * 2 + this.pizzaSize);
 }
 
-PapiPizza.prototype.calculateTopping = function() {
-  var toppingCost = 0;
-  totalCost = this.pizzaToppings.length;
-  return toppingCost;
-}
 
-PapiPizza.prototype.calculateFinalCost = function() {
-  finalCost = pizzaSizePrice + toppingCost;
-  return finalCost;
-  console.log(finalCost);
-}
-
-// fron end
+// front end
 $(document).ready(function() {
   $("form#papiOrder").submit(function(event) {
+    $("#pizzaOutput").empty();
+    $("#finalToppings").empty();
+
    event.preventDefault();
    debugger;
    var finalToppings = [];
-   var toppingArray = ["top1", "top2", "top3"];
-
-   toppingArray.forEach(function(pizza) {
-      var checkBox = ("#" + pizza).toString();
-      var value = $(checkBox).val();
-      var checked = $(checkBox).is(':checked');
-      if (checked === true) {
-        finalToppings.push(value);
-      }
-    });
-    console.log(finalToppings);
-
-   var pizzaSize = $("input:radio[name=size]:checked").val();
-    return  $(this).val();
-
-
-//    var pizzaTops1 = $("input:checkbox[name=pizzaTops]:checked").each(function(){
-//     finalToppings.push($(this).val());
-// });
-
-    console.log(finalToppings);
-     console.log(pizzaSize);
+   var pizzaSize = parseInt($("input:radio[name=size]:checked").val());
+   $('input[type=checkbox]').each(function(){
+         if (this.checked) {
+           finalToppings.push($(this).val());
+     }
+   });
 
 
    newPapiPizza = new PapiPizza(pizzaSize, finalToppings);
-   pizzaSize = newPapiPizza.prototype.calculateSize();
-  finalToppings = newPapiPizza.prototype.calculateTopping();
-   finalPizza =newPapiPizza.prototype.calculateFinalCost();
-    });
-
-   $("#sizePrice").html(pizzaSize);
-   $("#toppingPrice").html(finalToppings);
-   $("#finalPrice").html(finalPizza);
- });
+   $("#pizzaOutput").append("<p> Papi Thanks you for choosing us, the total comes too: $" + newPapiPizza.calculatePizzaPrice() + "</p>");
+      finalToppings.forEach(function(finalPizzaTopping){
+        $("#toppingOutput").append("<li>" + finalPizzaTopping + "</li>");
+          $(".finalToppings").show();
+      });
+      console.log(newPapiPizza);
+      console.log(pizzaSize);
+  });
+});
